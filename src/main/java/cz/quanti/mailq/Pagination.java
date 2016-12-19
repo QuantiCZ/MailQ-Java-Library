857a1f683a6325379d200b93c34836d43ac0b1dd
blazekv <vladimir.blazek@quanti.cz>
2016-12-19 15:39:56 +0100

    Pagination default object

--- src/main/java/cz/quanti/mailq/Pagination.java
+++ src/main/java/cz/quanti/mailq/Pagination.java
@@ -6,9 +6,15 @@ public class Pagination {
     private Long limit;
     private Long offset;
 
+    public static final Pagination DEFAULT = new Pagination(100L,0L);
+
     public Pagination(Long limit, Long offset) {
         this.limit = limit;
         this.offset = offset;
+    }
+
+    public static Pagination create(Long limit, Long offset) {
+        return new Pagination(limit,offset);
     }
 
     public Long getLimit() {
